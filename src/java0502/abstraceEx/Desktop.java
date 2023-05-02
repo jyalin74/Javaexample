b392fd424e71ae5df255fefb22a86c4d93ff3437
jyalin74 <108442547+jyalin74@users.noreply.github.com>
2023-05-02 15:53:02 +0900

    Initial commit

---
+++ src/java0502/abstraceEx/Desktop.java
@@ -0,0 +1,23 @@
+package java0502.abstraceEx;
+
+public class Desktop extends Computer{
+    @Override
+    public void display() {
+
+    }
+
+    @Override
+    public void typing() {
+
+    }
+
+    @Override
+    public void turnOn() {
+        super.turnOn();
+    }
+
+    @Override
+    public void turnOff() {
+        super.turnOff();
+    }
+}
